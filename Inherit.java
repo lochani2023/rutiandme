5232d8327130c0f2bf5ea4323be39fa6ec988dd2
bharambe2023 <rutujabharambe7@gmail.com>
2024-04-09 16:16:53 +0530

    new added

---
+++ Inherit.java
@@ -0,0 +1,20 @@
+class Parent
+{
+  Parent()
+  {
+	
+	System.out.println("parent class");
+  }
+}
+class Child extends Parent
+{
+	System.out.println("child class");
+}
+class Inherit
+{
+public static void main(String[] args)
+{
+	System.out.println("hi it's rutuja");
+	Child c=new Child();
+}
+}
